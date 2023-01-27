f31bb7461f1d08c60482f7af27fa7dd851fbcaad
Bryan Rodriguez <Serphp@hotmail.com>
2023-01-27 13:57:01 -0500

    PHP Init

--- create.php
+++ create.php
@@ -1,29 +1,47 @@
 <?php require "includes/header.php"; ?>
 <?php require "config.php"; ?>
 
+<?php 
+
+if(isset($_POST["submit"])){
+    if($_POST['title'] == '' OR $_POST['description'] == ''){
+        echo 'no title or description.';
+    }
+    else {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $username = $_POST['username'];
+        //conect
+        $insert = $conn->prepare("INSERT INTO posts(title, description, username) 
+        VALUES (:title, :description, :username)");
+
+        $insert->execute([
+            ':title' => $title,
+            ':description' => $description,
+            ':username' => $username
+        ]);
+    }
+}
+
 ?>
 <main class="form-signin w-50 m-auto">
     <form method="POST" action="register.php">
         <!-- <img class="mb-4 text-center" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
+        <div class="container mb-5">
         <h1 class="h3 mt-5 fw-normal text-center"> Create Post </h1>
+        </div>  
 
-        <div class="form-floating">
-            <input name="title" type="title" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <div class="form-floating mb-3">
+            <input name="title" type="title" class="form-control" id="floatingInput" placeholder="title">
             <label for="floatingInput">Title</label>
         </div>
 
-        <div class="form-floating">
-            <textarea name="" type="text" class="form-control" id="floatingInput" placeholder="Body"></textarea>
+        <div class="form-floating mb-3">
+            <textarea name="description" type="text" class="form-control" id="floatingInput" placeholder="Body"></textarea>
             <label for="floatingInput"> description </label>
         </div>
 
-        <div class="form-floating">
-            <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-        </div>
-
-        <button name="submit" class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <h6 class="mt-3">Aleardy have an account? <a href="login.php">Login</a></h6>
+        <button name="submit" class="w-100 btn btn-lg btn-primary" type="submit"> Post </button>
 
     </form>
 </main>
